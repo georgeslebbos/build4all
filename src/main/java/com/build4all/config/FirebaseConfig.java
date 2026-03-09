2093b4a52e6404eb183060b9e6b12454e4c45be7
Fatima Kassem <kassemf96@gmail.com>
2026-03-09 11:55:08 +0200

    Recreate commit without Firebase key

--- src/main/java/com/build4all/config/FirebaseConfig.java
+++ src/main/java/com/build4all/config/FirebaseConfig.java
@@ -6,8 +6,8 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -26,7 +26,7 @@ public class FirebaseConfig {
             return FirebaseApp.getInstance();
         }
 
-        try (InputStream inputStream = new FileInputStream(serviceAccountPath)) {
+        try (InputStream inputStream = new ClassPathResource(serviceAccountPath).getInputStream()) {
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(inputStream))
                     .setProjectId(projectId)
